fa6038498795f92dcde32d0bec97bd70432de650
Sunny Capt <sunny.capt@tuta.io>
2024-05-06 20:48:23 +0200

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,6 +2,7 @@ package main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(3);
+        System.out.println(7);
+        System.out.println("SunnyCapt was here");
     }
 }
